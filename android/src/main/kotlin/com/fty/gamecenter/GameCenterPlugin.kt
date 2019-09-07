39e1c1356751b721cf605b11aa5428a8dfa28ad3
Yasin Uğur Yıldız <ugur.yildiz@fty.com.tr>
2019-09-08 00:46:11 +0300

    Update

--- android/src/main/kotlin/com/fty/gamecenter/GameCenterPlugin.kt
+++ android/src/main/kotlin/com/fty/gamecenter/GameCenterPlugin.kt
@@ -7,70 +7,80 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import io.flutter.embedding.android.FlutterFragment.Builder
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.OnCompleteListener
-import java.io.IOException
-import android.system.Os.close
-import android.graphics.Bitmap
-import java.io.FileOutputStream
-import java.util.UUID
-import java.io.File
-import java.io.OutputStream
-import android.os.Environment
-import androidx.core.app.NotificationCompat.getExtras
-import android.os.Bundle
 import android.content.Intent
 import android.util.Log
 import io.flutter.plugin.common.PluginRegistry
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
 
 
-class GameCenterPlugin : MethodCallHandler {
+class GameCenterPlugin(private val registrar: Registrar) : MethodCallHandler, ActivityResultListener {
 
-    private final val activity: Activity
+    private lateinit var result: Result
 
-    constructor(activity: Activity) {
-        this.activity = activity
-    }
+    companion object {
 
-
-    companion object : PluginRegistry.ActivityResultListener {
-        override fun onActivityResult(p0: Int, p1: Int, p2: Intent?): Boolean {
-            Log.v("onActivityResult", "$p0 $p1")
-            return true
-        }
+        private const val RC_SIGN_IN = 9001
 
         @JvmStatic
         fun registerWith(registrar: Registrar) {
+            val plugin = GameCenterPlugin(registrar)
+            registrar.addActivityResultListener(plugin)
             val channel = MethodChannel(registrar.messenger(), "gamecenter")
-            channel.setMethodCallHandler(GameCenterPlugin(registrar.activity()))
-            registrar.addActivityResultListener(this)
+            channel.setMethodCallHandler(plugin)
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?): Boolean {
+        Log.v("onMethodCall", "addActivityResultListener")
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                Log.v("onActivityResult", "$requestCode $resultCode ${Activity.RESULT_OK}")
+                val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(intent)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    result.success(true)
+
+                } catch (apiException: ApiException) {
+                    Log.v("ApiException", apiException.toString())
+                    result.success(false)
+                }
+            } else {
+                Log.v("onMethodCall", false.toString())
+                result.success(false)
+            }
+
+        }
+        return true
+    }
+
     override fun onMethodCall(call: MethodCall, result: Result) {
+
+        this.result = result
+
         if (call.method == "login") {
+
             val silent = call.arguments as Boolean
 
-            android.util.Log.v("Silent", silent.toString())
-
-            val mGoogleSignInClient = GoogleSignIn.getClient(this.activity, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+            val mGoogleSignInClient = GoogleSignIn.getClient(this.registrar.activity(), GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
 
             if (silent) {
-                mGoogleSignInClient.silentSignIn().addOnCompleteListener(this.activity,
+                mGoogleSignInClient.silentSignIn().addOnCompleteListener(this.registrar.activity(),
                         OnCompleteListener<GoogleSignInAccount> { task ->
                             android.util.Log.v("OnCompleteListener", task.isSuccessful.toString())
                             if (task.isSuccessful) {
                                 result.success(true)
                             } else {
-                                result.success(false)
+                                result.success(task.exception!!.message)
                                 //onDisconnected()
                             }
                         })
             } else {
-                this.activity.startActivityForResult(mGoogleSignInClient.signInIntent, 9001)
+                this.registrar.activity().startActivityForResult(mGoogleSignInClient.signInIntent, RC_SIGN_IN)
             }
 
 
